046d49a3e0a47a80993d932d5a7ab30f61a13901
Julian Karhof <julian@karhof.net>
2023-08-01 22:37:35 +0200

    dont float nvim-tree

--- .config/nvim/lua/conf/nvim-tree.lua
+++ .config/nvim/lua/conf/nvim-tree.lua
@@ -11,7 +11,7 @@ require('nvim-tree').setup {
   view = {
     centralize_selection = true,
     float = {
-      enable = true,
+      enable = false,
       quit_on_focus_loss = true,
       open_win_config = {
         border = "none",
